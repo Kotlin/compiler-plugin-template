a0b302f4a214320011cede79f14b9471358d191d
Youssef Shoaib <canonballt@gmail.com>
2026-08-04 22:07:44 +0100

    Use KMP DevKit

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,7 +8,7 @@ pluginManagement {
 }
 
 plugins {
-    kotlin("compiler.plugin.devkit") version "0.0.1-SNAPSHOT"
+    kotlin("compiler.plugin.devkit") version "0.0.2-SNAPSHOT"
 }
 
 dependencyResolutionManagement {
