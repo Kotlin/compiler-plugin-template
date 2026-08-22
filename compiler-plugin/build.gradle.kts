3a32c2b999dc524aa9c40101477918e09e6b7584
Youssef Shoaib <canonballt@gmail.com>
2026-08-22 17:19:03 +0100

    Use versionHierarchy dsl

--- compiler-plugin/build.gradle.kts
+++ compiler-plugin/build.gradle.kts
@@ -1,18 +1,14 @@
-import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
-
 plugins {
     pluginDevKit("compiler-plugin")
     alias(libs.plugins.gradle.maven.publish)
 }
 
-@OptIn(ExperimentalKotlinGradlePluginApi::class)
 pluginDevKit {
-    kotlin {
-        applyPluginDevKitHierarchyTemplate {
-            postDev(2, 5, "post25Dev")
-            preDev(2, 5, "pre25Dev")
-        }
+    versionHierarchy {
+        splitDev(2, 5)
     }
-    componentRegistrar = "org.jetbrains.kotlin.compiler.plugin.template.SimplePluginComponentRegistrar"
-    commandLineProcessor = "org.jetbrains.kotlin.compiler.plugin.template.SimpleCommandLineProcessor"
+    componentRegistrar =
+        "org.jetbrains.kotlin.compiler.plugin.template.SimplePluginComponentRegistrar"
+    commandLineProcessor =
+        "org.jetbrains.kotlin.compiler.plugin.template.SimpleCommandLineProcessor"
 }
